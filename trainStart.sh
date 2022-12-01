d5076507567233b54d2d7b84d7e679cdb9bde6f8
sheins <slav1195@yandex.ru>
2022-12-01 18:07:18 +0300

    New train with new env and LSTM

--- trainStart.sh
+++ trainStart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -J ppo_env4
+#SBATCH -J ppo_env10
 #SBATCH -D /s/ls4/users/slava1195/rl_rob/RL_robotSim
 #SBATCH -o /s/ls4/users/slava1195/rl_rob/RL_robotSim/Logs/Continuous_%x_%j.out
 #SBATCH -e /s/ls4/users/slava1195/rl_rob/RL_robotSim/Logs/Continuous_%x_%j.err
