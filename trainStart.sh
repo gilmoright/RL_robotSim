e60fd0a7deff29a4c6f91693014cb4e9ff2cb2c4
sheins <slav1195@yandex.ru>
2022-12-02 12:38:45 +0300

    New configs train (10v2)

--- trainStart.sh
+++ trainStart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -J ppo_env10
+#SBATCH -J ppo_env10v2
 #SBATCH -D /s/ls4/users/slava1195/rl_rob/RL_robotSim
 #SBATCH -o /s/ls4/users/slava1195/rl_rob/RL_robotSim/Logs/Continuous_%x_%j.out
 #SBATCH -e /s/ls4/users/slava1195/rl_rob/RL_robotSim/Logs/Continuous_%x_%j.err
