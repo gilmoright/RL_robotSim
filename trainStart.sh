827e537d766cd573b49988a133b00f92c81fc3d9
SheinSlava <slav1195@yandex.ru>
2022-12-04 18:02:56 +0300

    New experiments with env 12 and env 13

--- trainStart.sh
+++ trainStart.sh
@@ -4,7 +4,7 @@
 #SBATCH -D /s/ls4/users/slava1195/rl_rob/RL_robotSim
 #SBATCH -o /s/ls4/users/slava1195/rl_rob/RL_robotSim/Logs/Continuous_%x_%j.out
 #SBATCH -e /s/ls4/users/slava1195/rl_rob/RL_robotSim/Logs/Continuous_%x_%j.err
-#SBATCH -p hpc5-el7-gpu-3d
+#SBATCH -p hpc4-el7-gpu-3d
 #SBATCH -n 16
 #SBATCH -N 1
 #SBATCH --gres=gpu:k80:6
