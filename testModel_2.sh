54cf1dd2f168653ee727c8ad921a0a15a6005d93
sheins <slav1195@yandex.ru>
2022-12-16 11:42:58 +0300

    Test 19v1 35

--- testModel_2.sh
+++ testModel_2.sh
@@ -14,30 +14,31 @@ export PATH=$HOME/anaconda3/envs/rl_robots/bin:$PATH
 
 export LD_LIBRARY_PATH=/s/ls4/sw/cuda/10.1/lib64:/s/ls4/sw/cuda/10.1/nvvm/lib64:$HOME/installation_dists/cudnn-9.0-linux-x64-v7.1.ga/lib64:
 
-CONFIG_DIR=/s/ls4/users/slava1195/rl_rob/RL_robotSim/results/FollowerContinuous/env19/PPO/feats_v12_train_35/ppo_env19v1_feats_v12_train5v7/PPO_continuous-grid_45046_00000_0_2022-12-15_16-34-59
-RUN_DIR=/s/ls4/users/slava1195/rl_rob/RL_robotSim/results/FollowerContinuous/env19/PPO/feats_v12_train_70/ppo_env19v3_feats_v12_train5v7/PPO_continuous-grid_5c8cb_00001_1_2022-12-14_12-19-29
+#CONFIG_DIR=/s/ls4/users/slava1195/rl_rob/RL_robotSim/results/FollowerContinuous/env19/PPO/feats_v12_train_35/ppo_env19v1_feats_v12_train5v7/PPO_continuous-grid_45046_00000_0_2022-12-15_16-34-59
+#RUN_DIR=/s/ls4/users/slava1195/rl_rob/RL_robotSim/results/FollowerContinuous/env19/PPO/feats_v12_train_70/ppo_env19v3_feats_v12_train5v7/PPO_continuous-grid_5c8cb_00001_1_2022-12-14_12-19-29
 #CONFIG_DIR=/s/ls4/users/slava1195/rl_rob/RL_robotSim/results/FollowerContinuous/env19/PPO/feats_v12_train_35/ppo_env19v2_feats_v12_train5v7/PPO_continuous-grid_5c8cb_00000_0_2022-12-14_12-19-08
-#RUN_DIR=/s/ls4/users/slava1195/rl_rob/RL_robotSim/results/FollowerContinuous/env19/PPO/feats_v12_train_70/ppo_env19v4_feats_v12_train5v7/PPO_continuous-grid_5c8cb_00002_2_2022-12-14_12-19-46
+RUN_DIR=/s/ls4/users/slava1195/rl_rob/RL_robotSim/results/FollowerContinuous/env19/PPO/feats_v12_train_70/ppo_env19v4_feats_v12_train5v7/PPO_continuous-grid_5c8cb_00002_2_2022-12-14_12-19-46
+CONFIG_DIR = RUN_DIR
 
 python TestModel_2.py --rlalgo PPO \
 --run_dir $RUN_DIR \
 --config_dir $CONFIG_DIR \
---checkpoint_number 290
+--checkpoint_number 380
 
 python TestModel_2.py --rlalgo PPO \
 --run_dir $RUN_DIR \
 --config_dir $CONFIG_DIR \
---checkpoint_number 360
+--checkpoint_number 340
 
 python TestModel_2.py --rlalgo PPO \
 --run_dir $RUN_DIR \
 --config_dir $CONFIG_DIR \
---checkpoint_number 180
+--checkpoint_number 140
 
 python TestModel_2.py --rlalgo PPO \
 --run_dir $RUN_DIR \
 --config_dir $CONFIG_DIR \
---checkpoint_number 330
+--checkpoint_number 200
 #
 #python TestModel.py --rlalgo PPO \
 #--run_dir $RUN_DIR \
