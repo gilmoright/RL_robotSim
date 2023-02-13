225e39b07efd47673fd108694c5949944f7070b5
sheins <slav1195@yandex.ru>
2023-02-13 14:06:52 +0300

    New train 13.02.23

--- trainStart.sh
+++ trainStart.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH -J ppo_env12
+#SBATCH -J ppo_env123
 #SBATCH -D /s/ls4/users/slava1195/rl_rob/RL_robotSim
 #SBATCH -o /s/ls4/users/slava1195/rl_rob/RL_robotSim/Logs/Continuous_%x_%j.out
 #SBATCH -e /s/ls4/users/slava1195/rl_rob/RL_robotSim/Logs/Continuous_%x_%j.err
-#SBATCH -p hpc4-el7-gpu-3d
-#SBATCH -n 12
-#SBATCH --gres=gpu:k80:3
+#SBATCH -p hpc5-el7-gpu-3d
+#SBATCH -n 16
+#SBATCH --gres=gpu:k80:4
 #SBATCH --time=72:00:00
 
 export HOME=/s/ls4/users/slava1195
@@ -78,7 +78,9 @@ export LD_LIBRARY_PATH=/s/ls4/sw/cuda/10.1/lib64:/s/ls4/sw/cuda/10.1/nvvm/lib64:
 ##### 10.02.2023
 #python MyTrain.py --config-file Configs/FollowerContinuous/PPO_dyn_obst.conf --experiments ppo_env22v1_feats_v15_fi_lstmv7v2 ppo_env22v2_feats_v15_fi_lstmv7v2 ppo_env22v1_feats_v15_fi_v5v7_sqd
 
-#### 12.02.2023
-python MyTrain.py --config-file Configs/FollowerContinuous/PPO_dyn_obst.conf --experiments ppo_dv2_env22v1_feats_v15_fi_lstmv7v2 ppo_dv2_env22v2_feats_v15_fi_lstmv7v2 ppo_dv2_env22v1_feats_v15_fi_v5v7_sqd
+##### 12.02.2023
+#python MyTrain.py --config-file Configs/FollowerContinuous/PPO_dyn_obst.conf --experiments ppo_dv2_env22v1_feats_v15_fi_lstmv7v2 ppo_dv2_env22v2_feats_v15_fi_lstmv7v2 ppo_dv2_env22v1_feats_v15_fi_v5v7_sqd
 
+#### 13.02.2023
+python MyTrain.py --config-file Configs/FollowerContinuous/PPO_dyn_obst.conf --experiments ppo_sv1_env23v1_feats_v15_fi_lstm_v10v1 ppo_sv1_env23v2_feats_v15_fi_lstm_v10_v2 ppo_sv1_env23v1_feats_v15_fi_v5v8_sqd
 
