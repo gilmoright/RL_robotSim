cf939957896c0b2f7358a2d533564d1a1d364c32
SheinSlava <slav1195@yandex.ru>
2023-04-04 16:56:24 +0300

    New custom transformer model (04/04/23)

--- MyTfModel.py
+++ MyTfModel.py
@@ -127,4 +127,66 @@ class MyTransformerModel(TFModelV2):
         return tf.reshape(self._value_out, [-1])
 
 
-ModelCatalog.register_custom_model("transformer_model", MyTransformerModel)
+# ModelCatalog.register_custom_model("transformer_model", MyTransformerModel)
+
+class MyFrameStackingModel(TFModelV2):
+    """A simple FC model that takes the last n observations as input."""
+
+    def __init__(self,
+                 obs_space,
+                 action_space,
+                 num_outputs,
+                 model_config,
+                 name,
+                 num_frames=3):
+        super(MyFrameStackingModel, self).__init__(
+            obs_space, action_space, None, model_config, name)
+
+        self.num_frames = num_frames
+        self.num_outputs = num_outputs
+
+        # Construct actual (very simple) FC model.
+        assert len(obs_space.shape) == 1
+        obs = tf.keras.layers.Input(
+            shape=(self.num_frames, obs_space.shape[0]))
+        obs_reshaped = tf.keras.layers.Reshape(
+            [obs_space.shape[0] * self.num_frames])(obs)
+        rewards = tf.keras.layers.Input(shape=(self.num_frames))
+        rewards_reshaped = tf.keras.layers.Reshape([self.num_frames])(rewards)
+        actions = tf.keras.layers.Input(
+            shape=(self.num_frames, self.action_space.n))
+        actions_reshaped = tf.keras.layers.Reshape(
+            [action_space.n * self.num_frames])(actions)
+        input_ = tf.keras.layers.Concatenate(axis=-1)(
+            [obs_reshaped, actions_reshaped, rewards_reshaped])
+        layer1 = tf.keras.layers.Dense(256, activation=tf.nn.relu)(input_)
+        layer2 = tf.keras.layers.Dense(256, activation=tf.nn.relu)(layer1)
+        out = tf.keras.layers.Dense(self.num_outputs)(layer2)
+        values = tf.keras.layers.Dense(1)(layer1)
+        self.base_model = tf.keras.models.Model([obs, actions, rewards],
+                                                [out, values])
+        self._last_value = None
+
+        self.view_requirements["prev_n_obs"] = ViewRequirement(
+            data_col="obs",
+            shift="-{}:0".format(num_frames - 1),
+            space=obs_space)
+        self.view_requirements["prev_n_rewards"] = ViewRequirement(
+            data_col="rewards", shift="-{}:-1".format(self.num_frames))
+        self.view_requirements["prev_n_actions"] = ViewRequirement(
+            data_col="actions",
+            shift="-{}:-1".format(self.num_frames),
+            space=self.action_space)
+
+    def forward(self, input_dict, states, seq_lens):
+        obs = tf.cast(input_dict["prev_n_obs"], tf.float32)
+        rewards = tf.cast(input_dict["prev_n_rewards"], tf.float32)
+        actions = one_hot(input_dict["prev_n_actions"], self.action_space)
+        print(obs, obs.shape)
+        out, self._last_value = self.base_model([obs, actions, rewards])
+        return out, []
+
+    def value_function(self):
+        return tf.squeeze(self._last_value, -1)
+
+ModelCatalog.register_custom_model("transformer_model", MyFrameStackingModel)
