a0f8006eecfb1ad70b7a75591fe2f96bb03454c7
SheinSlava <slav1195@yandex.ru>
2023-04-04 17:09:00 +0300

    New custom transformer model (04/04/23)

--- MyTfModel.py
+++ MyTfModel.py
@@ -129,150 +129,39 @@ class MyTransformerModel(TFModelV2):
 
 # ModelCatalog.register_custom_model("transformer_model", MyTransformerModel)
 
-class MyFCNetFrames(TFModelV2):
-    """Generic fully connected network implemented in ModelV2 API."""
+class MyKerasModel(TFModelV2):
+    """Custom model for policy gradient algorithms."""
 
-    def __init__(self, obs_space: gym.spaces.Space,
-                 action_space: gym.spaces.Space, num_outputs: int,
-                 model_config: ModelConfigDict, name: str):
-        super(MyFCNetFrames, self).__init__(
-            obs_space, action_space, num_outputs, model_config, name)
-        self.num_frames = 3
-
-        hiddens = list(model_config.get("fcnet_hiddens", [])) + \
-            list(model_config.get("post_fcnet_hiddens", []))
-        activation = model_config.get("fcnet_activation")
-        if not model_config.get("fcnet_hiddens", []):
-            activation = model_config.get("post_fcnet_activation")
-        activation = get_activation_fn(activation)
-        no_final_linear = model_config.get("no_final_linear")
-        vf_share_layers = model_config.get("vf_share_layers")
-        free_log_std = model_config.get("free_log_std")
-
-        # Generate free-floating bias variables for the second half of
-        # the outputs.
-        if free_log_std:
-            assert num_outputs % 2 == 0, (
-                "num_outputs must be divisible by two", num_outputs)
-            num_outputs = num_outputs // 2
-            self.log_std_var = tf.Variable(
-                [0.0] * num_outputs, dtype=tf.float32, name="log_std")
-
-        # We are using obs_flat, so take the flattened shape as input.
-        inputs = tf.keras.layers.Input(
-            #shape=(self.num_frames, obs_space.shape[0], obs_space.shape[1], ), name="observations")
-            #shape=(int(np.product(obs_space.shape)), ), name="observations")
-            shape=(self.num_frames, int(np.product(obs_space.shape)), ), name="observations")
-        obs_reshaped = tf.keras.layers.Reshape([int(np.product(obs_space.shape))*self.num_frames])(inputs)
-        # Last hidden layer output (before logits outputs).
-        last_layer = obs_reshaped
-        # The action distribution outputs.
-        logits_out = None
-        i = 1
-
-        # Create layers 0 to second-last.
-        for size in hiddens[:-1]:
-            last_layer = tf.keras.layers.Dense(
-                size,
-                name="fc_{}".format(i),
-                activation=activation,
-                kernel_initializer=normc_initializer(1.0))(last_layer)
-            i += 1
-
-        # The last layer is adjusted to be of size num_outputs, but it's a
-        # layer with activation.
-        if no_final_linear and num_outputs:
-            logits_out = tf.keras.layers.Dense(
-                num_outputs,
-                name="fc_out",
-                activation=activation,
-                kernel_initializer=normc_initializer(1.0))(last_layer)
-        # Finish the layers with the provided sizes (`hiddens`), plus -
-        # iff num_outputs > 0 - a last linear layer of size num_outputs.
-        else:
-            if len(hiddens) > 0:
-                last_layer = tf.keras.layers.Dense(
-                    hiddens[-1],
-                    name="fc_{}".format(i),
-                    activation=activation,
-                    kernel_initializer=normc_initializer(1.0))(last_layer)
-            if num_outputs:
-                logits_out = tf.keras.layers.Dense(
-                    num_outputs,
-                    name="fc_out",
-                    activation=None,
-                    kernel_initializer=normc_initializer(0.01))(last_layer)
-            # Adjust num_outputs to be the number of nodes in the last layer.
-            else:
-                self.num_outputs = (
-                    [int(np.product(obs_space.shape))] + hiddens[-1:])[-1]
-
-        # Concat the log std vars to the end of the state-dependent means.
-        if free_log_std and logits_out is not None:
-
-            def tiled_log_std(x):
-                return tf.tile(
-                    tf.expand_dims(self.log_std_var, 0), [tf.shape(x)[0], 1])
-
-            log_std_out = tf.keras.layers.Lambda(tiled_log_std)(inputs)
-            logits_out = tf.keras.layers.Concatenate(axis=1)(
-                [logits_out, log_std_out])
-
-        last_vf_layer = None
-        if not vf_share_layers:
-            # Build a parallel set of hidden layers for the value net.
-            last_vf_layer = inputs
-            i = 1
-            for size in hiddens:
-                last_vf_layer = tf.keras.layers.Dense(
-                    size,
-                    name="fc_value_{}".format(i),
-                    activation=activation,
-                    kernel_initializer=normc_initializer(1.0))(last_vf_layer)
-                i += 1
-
+    def __init__(self, obs_space, action_space, num_outputs, model_config, name):
+        super(MyKerasModel, self).__init__(
+            obs_space, action_space, num_outputs, model_config, name
+        )
+        self.inputs = tf.keras.layers.Input(shape=obs_space.shape, name="observations")
+        layer_1 = tf.keras.layers.Dense(
+            128,
+            name="my_layer1",
+            activation=tf.nn.relu,
+            kernel_initializer=normc_initializer(1.0),
+        )(self.inputs)
+        layer_out = tf.keras.layers.Dense(
+            num_outputs,
+            name="my_out",
+            activation=None,
+            kernel_initializer=normc_initializer(0.01),
+        )(layer_1)
         value_out = tf.keras.layers.Dense(
             1,
             name="value_out",
             activation=None,
-            kernel_initializer=normc_initializer(0.01))(
-                last_vf_layer if last_vf_layer is not None else last_layer)
+            kernel_initializer=normc_initializer(0.01),
+        )(layer_1)
+        self.base_model = tf.keras.Model(self.inputs, [layer_out, value_out])
 
-        self.base_model = tf.keras.Model(
-            inputs, [(logits_out
-                      if logits_out is not None else last_layer), value_out])
-
-        self._last_value = None
-        self.view_requirements["prev_n_obs"] = ViewRequirement(
-                data_col="obs",
-                shift="-{}:0".format(self.num_frames-1),
-                space = obs_space
-            )
-        #self.view_requirements["prev_n_rewards"] = ViewRequirement(
-        #        data_col="rewards",
-        #        shift="-{}:-1".format(self.num_frames)
-        #    )
-        #self.view_requirements["prev_n_actions"] = ViewRequirement(
-        #        data_col="actions",
-        #        shift="-{}:-1".format(self.num_frames),
-        #        space=self.action_space
-        #    )
-
-    def forward(self, input_dict: Dict[str, TensorType],
-                state: List[TensorType],
-                seq_lens: TensorType) -> (TensorType, List[TensorType]):
-        print(input_dict.keys())
-        print(input_dict["obs"].shape)
-        print(input_dict["prev_n_obs"].shape)
-        print(input_dict["obs_flat"].shape)
-        print(self.view_requirements)
-        print(self.__class__)
-        obs = tf.cast(input_dict["prev_n_obs"], tf.float32)
-        #model_out, self._value_out = self.base_model(input_dict["obs_flat"])
-        model_out, self._value_out = self.base_model(obs)
+    def forward(self, input_dict, state, seq_lens):
+        model_out, self._value_out = self.base_model(input_dict["obs"])
         return model_out, state
 
-    def value_function(self) -> TensorType:
+    def value_function(self):
         return tf.reshape(self._value_out, [-1])
 
-ModelCatalog.register_custom_model("transformer_model", MyFCNetFrames)
+ModelCatalog.register_custom_model("transformer_model", MyKerasModel)
