bdbc36f2f4e6fc2a2791c5348cb45d40ded7ec60
SheinSlava <slav1195@yandex.ru>
2023-04-04 15:15:21 +0300

    New custom transformer model (04/04/23)

--- trainStart.sh
+++ trainStart.sh
@@ -5,8 +5,8 @@
 #SBATCH -o /s/ls4/users/slava1195/rl_rob/RL_robotSim/Logs/Continuous_%x_%j.out
 #SBATCH -e /s/ls4/users/slava1195/rl_rob/RL_robotSim/Logs/Continuous_%x_%j.err
 #SBATCH -p hpc4-el7-gpu-3d
-#SBATCH -n 12
-#SBATCH --gres=gpu:k80:2
+#SBATCH -n 4
+#SBATCH --gres=gpu:k80:1
 #SBATCH --time=72:00:00
 
 export HOME=/s/ls4/users/slava1195
