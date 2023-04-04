0f7ce972ad2b8507603fc2bd036b5a881e6ea7f5
SheinSlava <slav1195@yandex.ru>
2023-04-04 18:17:03 +0300

    New custom transformer model (04/04/23)

--- MyTfModel.py
+++ MyTfModel.py
@@ -105,6 +105,7 @@ class MyTransformerModel(TFModelV2):
         # input_shape = obs_space.shape[0]
         input_shape = tf.keras.layers.Input(
             shape=obs_space.shape, name="observations")
+        print("AAAAAAAAAAAAAA", input_shape)
         self.base_model = self.build_model(input_shape, head_size=256, num_heads=4, ff_dim=4, num_transformer_blocks=4,
                                            mlp_units=[128], mlp_dropout=0.4, dropout=0.25, n_classes=2)
 
@@ -118,7 +119,7 @@ class MyTransformerModel(TFModelV2):
             x = tf.keras.layers.Dense(dim, activation="relu")(x)
             x = tf.keras.layers.Dropout(mlp_dropout)(x)
         outputs = tf.keras.layers.Dense(n_classes, activation="softmax")(x)
-        return tf.keras.Model(inputs, outputs)
+        return tf.keras.Model(input_shape, outputs)
 
 
 
