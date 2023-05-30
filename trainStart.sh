3d883b2b97648b820bbc5126f468f1c9f31570e0
gilmoright <artem.dahaka@gmail.com>
2023-05-30 18:57:26 +0300

    Добавил эксперименты с более правильным отображением сейф зона на 2д карте

--- trainStart.sh
+++ trainStart.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH -J ppo_env4
+#SBATCH -J ppo_env4_feats13
 #SBATCH -D /s/ls4/users/grartem/RL_robots/RL_robotSim
 #SBATCH -o /s/ls4/users/grartem/RL_robots/RL_robotSim/Logs/Continuous_%x_%j.out
 #SBATCH -e /s/ls4/users/grartem/RL_robots/RL_robotSim/Logs/Continuous_%x_%j.err
-#SBATCH -p hpc5-el7-gpu-3d
-#SBATCH -n 12
-#SBATCH --gres=gpu:k80:3
-#SBATCH --time=72:00:00
+#SBATCH -p hpc4-el7-gpu-3d
+#SBATCH -n 8
+#SBATCH --gres=gpu:k80:2
+#SBATCH --time=48:00:00
 
 export HOME=/s/ls4/users/grartem
 export PATH=$HOME/anaconda3/envs/rl_robots/bin:$PATH
@@ -17,5 +17,5 @@ export LD_LIBRARY_PATH=/s/ls4/sw/cuda/10.1/lib64:/s/ls4/sw/cuda/10.1/nvvm/lib64:
 #python MyTrain.py --config-file Configs/FollowerContinuous/TD3.yml
 #python MyTrain.py --config-file Configs/FollowerContinuous/TD3_obst.conf --experiments td3_algov3 td3_algov5 td3_algov6
 #python MyTrain.py --config-file Configs/FollowerContinuous/A3C_obstDiscr.conf --experiments a3c_arch6 a3c_feats2 a3c_feats1
-python MyTrain.py --config-file Configs/FollowerContinuous/PPO_obst.conf --experiments ppo_env4_feats12 ppo_env4feats12_train5v2 ppo_env4feats12_train5v6
+python MyTrain.py --config-file Configs/FollowerContinuous/PPO_obst.conf --experiments ppo_env4_feats13v6 ppo_env4_feats13v5
 # ppo_env4_feats11
