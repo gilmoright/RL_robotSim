5451975c37a8715e3f76c42519ea7e492e321820
gilmoright <artem.dahaka@gmail.com>
2023-07-24 19:53:22 +0300

    Провел эксперименты, чтоб повторить славину последнюю таблицу из отчета с двумя медведями + свой трансформер

--- testModel.sh
+++ testModel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -J testppoE10F12
+#SBATCH -J ppo_e29_b2_f14VPC_mv4_mtransv4v5_tv5v16
 #SBATCH -D /s/ls4/users/grartem/RL_robots/RL_robotSim
 #SBATCH -o /s/ls4/users/grartem/RL_robots/RL_robotSim/Logs/%x_%j.out
 #SBATCH -e /s/ls4/users/grartem/RL_robots/RL_robotSim/Logs/%x_%j.err
@@ -15,39 +15,16 @@ export PATH=$HOME/anaconda3/envs/rl_robots/bin:$PATH
 export LD_LIBRARY_PATH=/s/ls4/sw/cuda/10.1/lib64:/s/ls4/sw/cuda/10.1/nvvm/lib64:$HOME/installation_dists/cudnn-9.0-linux-x64-v7.1.ga/lib64:
 
 
-RUN_DIR_1=/s/ls4/users/slava1195/rl_rob_2/RL_robotSim/results/FollowerContinuous_test_sensors/env29/PPO/default_model/feats_v14VPC/bear2/ppo_e29_b1_f14VPC_mv4/PPO_continuous-grid_fded9_00000_0_2023-06-24_16-35-32
+RUN_DIR_1=/s/ls4/users/grartem/RL_robots/RL_robotSim/results/FollowerContinuous_test_sensors/env29/PPO/trans/feats_v14VPC/bear2/ppo_e29_b2_f14VPC_mv4_mtransv4v5_tv5v16/PPO_continuous-grid_e56c7_00001_1_2023-07-20_18-06-05
 
-RUN_DIR_2=/s/ls4/users/slava1195/rl_rob_2/RL_robotSim/results/FollowerContinuous_test_sensors/env29/PPO/default_model/feats_v14VPC/bear2/ppo_e29_b2_2_f14VPC_ar/PPO_continuous-grid_fded9_00001_1_2023-06-24_16-35-51
-
-RUN_DIR_3=/s/ls4/users/slava1195/rl_rob_2/RL_robotSim/results/FollowerContinuous_test_sensors/env29/PPO/default_model/feats_v14VPC/bear2/ppo_e29_b2_f14VPC_mv4/PPO_continuous-grid_fded9_00002_2_2023-06-24_16-36-10
-
-# feats14v2 bear 1 mv4
-python TestModel_2.py --rlalgo PPO \
+python TestModel.py --rlalgo PPO \
 --run_dir $RUN_DIR_1 \
---checkpoint_number 130
+--checkpoint_number 250
 
+python TestModel.py --rlalgo PPO \
+--run_dir $RUN_DIR_1 \
+--checkpoint_number 275
 
-
-# feats12v2 1 bear ar
-python TestModel_2.py --rlalgo PPO \
---run_dir $RUN_DIR_2 \
---checkpoint_number 95
-
-python TestModel_2.py --rlalgo PPO \
---run_dir $RUN_DIR_2 \
---checkpoint_number 135
-
-python TestModel_2.py --rlalgo PPO \
---run_dir $RUN_DIR_2 \
---checkpoint_number 115
-
-
-
-# feats12v2 1 bear mv4
-python TestModel_2.py --rlalgo PPO \
---run_dir $RUN_DIR_3 \
---checkpoint_number 130
-
-python TestModel_2.py --rlalgo PPO \
---run_dir $RUN_DIR_3 \
---checkpoint_number 175
+python TestModel.py --rlalgo PPO \
+--run_dir $RUN_DIR_1 \
+--checkpoint_number 300
--- trainStart.sh
+++ trainStart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -J ppo_env28
+#SBATCH -J ppo_e29_trv5v16
 #SBATCH -D /s/ls4/users/grartem/RL_robots/RL_robotSim
 #SBATCH -o /s/ls4/users/grartem/RL_robots/RL_robotSim/Logs/Continuous_%x_%j.out
 #SBATCH -e /s/ls4/users/grartem/RL_robots/RL_robotSim/Logs/Continuous_%x_%j.err
@@ -14,4 +14,4 @@ source activate rl_robots
 
 export LD_LIBRARY_PATH=/s/ls4/sw/cuda/10.1/lib64:/s/ls4/sw/cuda/10.1/nvvm/lib64:$HOME/installation_dists/cudnn-9.0-linux-x64-v7.1.ga/lib64:
 
-python MyTrain.py --config-file Configs/FollowerContinuous/PPO_dyn_obst_2.conf --experiments ppo_e29_b1_f14VPC_mv4 ppo_e29_b2_2_f14VPC_ar ppo_e29_b2_f14VPC_mv4
+python MyTrain.py --config-file Configs/FollowerContinuous/PPO_dyn_obst_2.conf --experiments ppo_e29_b2_f14VPC_mv4
