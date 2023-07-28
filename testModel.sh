9fabac53ee1c71fc8f213e7e58de91b5c1a10710
gilmoright <artem.dahaka@gmail.com>
2023-07-28 19:35:36 +0300

    Еще несколько экспериментов для сравнения славиных результатов с медведями и трансформера

--- testModel.sh
+++ testModel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -J ppo_e29_b2_f14VPC_mv4_mtransv4v5_tv5v16
+#SBATCH -J ppo_e29v2_b2_f14VPC_mv4
 #SBATCH -D /s/ls4/users/grartem/RL_robots/RL_robotSim
 #SBATCH -o /s/ls4/users/grartem/RL_robots/RL_robotSim/Logs/%x_%j.out
 #SBATCH -e /s/ls4/users/grartem/RL_robots/RL_robotSim/Logs/%x_%j.err
@@ -15,7 +15,7 @@ export PATH=$HOME/anaconda3/envs/rl_robots/bin:$PATH
 export LD_LIBRARY_PATH=/s/ls4/sw/cuda/10.1/lib64:/s/ls4/sw/cuda/10.1/nvvm/lib64:$HOME/installation_dists/cudnn-9.0-linux-x64-v7.1.ga/lib64:
 
 
-RUN_DIR_1=/s/ls4/users/grartem/RL_robots/RL_robotSim/results/FollowerContinuous_test_sensors/env29/PPO/trans/feats_v14VPC/bear2/ppo_e29_b2_f14VPC_mv4_mtransv4v5_tv5v16/PPO_continuous-grid_e56c7_00001_1_2023-07-20_18-06-05
+RUN_DIR_1=/s/ls4/users/grartem/RL_robots/RL_robotSim/results/FollowerContinuous_test_sensors/env29v2/PPO/default_model/feats_v14VPC/bear2/ppo_e29v2_b2_f14VPC_mv4/PPO_continuous-grid_e1e7b_00000_0_2023-07-25_16-40-57/
 
 python TestModel.py --rlalgo PPO \
 --run_dir $RUN_DIR_1 \
--- trainStart.sh
+++ trainStart.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH -J ppo_e29_trv5v16
+#SBATCH -J ppo_e29v2
 #SBATCH -D /s/ls4/users/grartem/RL_robots/RL_robotSim
 #SBATCH -o /s/ls4/users/grartem/RL_robots/RL_robotSim/Logs/Continuous_%x_%j.out
 #SBATCH -e /s/ls4/users/grartem/RL_robots/RL_robotSim/Logs/Continuous_%x_%j.err
 #SBATCH -p hpc5-el7-gpu-3d
-#SBATCH -n 4
-#SBATCH --gres=gpu:k80:1
+#SBATCH -n 8
+#SBATCH --gres=gpu:k80:2
 #SBATCH --time=72:00:00
 
 export HOME=/s/ls4/users/grartem
@@ -14,4 +14,4 @@ source activate rl_robots
 
 export LD_LIBRARY_PATH=/s/ls4/sw/cuda/10.1/lib64:/s/ls4/sw/cuda/10.1/nvvm/lib64:$HOME/installation_dists/cudnn-9.0-linux-x64-v7.1.ga/lib64:
 
-python MyTrain.py --config-file Configs/FollowerContinuous/PPO_dyn_obst_2.conf --experiments ppo_e29_b2_f14VPC_mv4
+python MyTrain.py --config-file Configs/FollowerContinuous/PPO_dyn_obst_2.conf --experiments ppo_e29v2_b2_f14VPC_mv4_tv5v20 ppo_e29v2_b2_f14VPC_mv4_mtransv4v5_tv5v19
